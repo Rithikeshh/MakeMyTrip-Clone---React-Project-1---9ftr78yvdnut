3eb402c16ff21ce07c5d50ce86a3d731d47b1beb
Rithikeshh <rithikeshh@gmail.com>
2023-10-28 02:30:29 +0530

    created header UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./styles/App.css";
+import allLogo from './assets/images/allLogos.png'
+import mmtLogo from './assets/images/mmtLogoWhite.png'
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/image-urls.js
@@ -0,0 +1,6 @@
+
+export  const imageUrls = {
+    makeMyTripLogo : '//imgak.mmtcdn.com/pwa_v3/pwa_hotel_assets/header/mmtLogoWhite.png',
+    allIcons : 'https://imgak.mmtcdn.com/pwa_v3/pwa_commons_assets/desktop/B2CHeaderSprite.png'
+
+}
