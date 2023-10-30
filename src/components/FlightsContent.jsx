ec959009b00c70e1d5a2e3db8fd31897e1428538
Rithikeshh <rithikeshh@gmail.com>
2023-10-30 14:26:51 +0530

    working on main content UI

--- src/components/FlightsContent.jsx
+++ src/components/FlightsContent.jsx
@@ -12,7 +12,7 @@ function FlightsContent() {
 
     const [value, setValue] = useState(1);
     const handleChange = (event) => {
-        setValue(event.target.value);
+      setValue(event.target.value);
     };
   return (
     <div>
@@ -20,18 +20,17 @@ function FlightsContent() {
         <div className='makeFlex gap-20 '>
             {checkboxForTickets.map((item,index)=>(
                 <FormControlLabel className={`${value==item.id && ('flights-ticket-active')} p-r-8`} key={index}
-                    control={
-                      <Checkbox
-                        checked={value==item.id}
-                        value={item.id}
-                        onChange={handleChange}
-                        inputProps={{ 'aria-label': 'controlled' }}
-                        defaultChecked
-                        sx={{ '& .MuiSvgIcon-root': { fontSize: 16 } }}
-                      />
-                    }
-                    
-                    label={item.name}
+                  control={
+                    <Checkbox
+                      checked={value==item.id}
+                      value={item.id}
+                      onChange={handleChange}
+                      inputProps={{ 'aria-label': 'controlled' }}
+                      defaultChecked
+                      sx={{ '& .MuiSvgIcon-root': { fontSize: 16 } }}
+                    />
+                  }
+                  label={item.name}
                 />
             ))}
         </div>
@@ -46,55 +45,109 @@ function FlightsContent() {
       </div>
       <div className='flight-booking-details-container'>
         <div className='flight-booking-details makeFlex make-align-center makeRelative'>
-            <div className='flight-inputBox searchFromCity'>
-              <label htmlFor="fromCity">
-                <span className='label-input margin-b-10'>From</span>
-                <input 
-                  className='flight-inputField lineHeight-36 font30 strongBold-text' 
-                  type="text" 
-                  id='fromCity'
-                  value={"Delhi"}
-                />
-                <p 
-                  className='makeRelative'
-                  title={'DEL, Delhi Airport India'}
-                >
-                  <span title='' className='airPortName'>
-                    {'DEL, Delhi Airport India'}
-                  </span>
-                </p>
-              </label>
-            </div>
-            <span className='flightSwapCircle'>
-              <span className='flightsSprite flightSwapIcon'></span>
-            </span>
-            <div className='flight-inputBox searchToCity'>
-              <label htmlFor="toCity">
-                <span className='label-input margin-b-10'>To</span>
-                <input 
-                  className='flight-inputField lineHeight-36 font30 strongBold-text' 
-                  type="text" 
-                  id='toCity'
-                  value={"Mumbai"}
-                />
-                <p 
-                  className='makeRelative'
-                  title={'BOM, Chhatrapati Shivaji International Airport India'}
-                >
-                  <span title='' className='airPortName'>
-                    {'BOM, Chhatrapati Shivaji International Airport India'}
-                  </span>
-                </p>
-              </label>
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
+          <InputBoxForCity 
+            mainDivClass='searchFromCity'
+            inputId = 'fromCity'
+            spanText = 'From'
+            paraTitle = {'DEL, Delhi Airport India'}
+            inputValue = {'Delhi'} 
+          />
+          <span className='flightSwapCircle'>
+            <span className='flightsSprite flightSwapIcon'></span>
+          </span>
+          <InputBoxForCity 
+            mainDivClass='searchToCity'
+            inputId = 'toCity'
+            spanText = 'To'
+            paraTitle = {'BOM, Chhatrapati Shivaji International Airport India'}
+            inputValue = {'Mumbai'}
+          />
+          <InputBoxForDate
+            mainDivClass='dates'
+            inputId = 'departure'            
+            spanText = 'Departure'
+            lastParaValue = 'Wednseday'
+          />
+          <InputBoxForDate
+            mainDivClass='dates'
+            inputId = 'return'            
+            spanText = 'Return'
+            lastParaValue = 'Friday'
+          />
+          <InputBoxForDate
+            mainDivClass='flightTravellers'
+            inputId = 'travellers'            
+            spanText = 'Travellers & Class'
+            lastParaValue = 'Business'
+          />
         </div>
         <div>
 
         </div>
       </div>
+    </div>
+  )
+}
+function InputBoxForCity({
+  mainDivClass,
+  inputId, 
+  spanText, 
+  paraTitle, 
+  inputValue
+}){
+  return(
+    <div className={`flight-inputBox ${mainDivClass}`}>
+      <label htmlFor={inputId}>
+        <span className='label-input margin-b-10'>{spanText}</span>
+        <input 
+          className='flight-inputField lineHeight-36 font30 strongBold-text' 
+          type="text" 
+          id={inputId}
+          value={inputValue}
+        />
+        <p 
+          className='makeRelative'
+          title={paraTitle}
+        >
+          <span title='' className='airPortName'>
+            {paraTitle}
+          </span>
+        </p>
+      </label>
+    </div>
+  )
+}
+
+function InputBoxForDate({
+  mainDivClass,
+  inputId,
+  spanText,
+  lastParaValue
+}){
+  return(
+    <div className={`flight-inputBox ${mainDivClass}`}>
+      <label htmlFor={inputId}>
+        <span className='label-input margin-b-10'>{spanText}</span>
+        <p className='lineHeight-36 font20'>
+          {mainDivClass == 'dates' && 
+          <><span className='font30 strongBold-text'>
+            {"1"}{" "}
+          </span>
+          <span>{"Nov"}</span>
+          <span className="shortYear">
+            {"23"}
+          </span></>}
+          {mainDivClass == 'flightTravellers' && 
+            <>
+              <span>
+                <span className='font30 strongBold-text'>{"4"}</span>
+                {" Travellers"}
+              </span>
+            </>
+          }
+        </p>
+        <p>{lastParaValue}</p>
+      </label>
     </div>
   )
 }
