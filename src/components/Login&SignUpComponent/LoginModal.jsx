679550205aeaa345132160efe8b401151a7663cf
Rithikeshh <rithikeshh@gmail.com>
2024-01-04 17:06:40 +0530

    All pages are responsive now

--- src/components/Login&SignUpComponent/LoginModal.jsx
+++ src/components/Login&SignUpComponent/LoginModal.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useRef} from 'react'
+import React,{useState, useRef, useEffect} from 'react'
 import LoginOrSignUpFormContainer from './LoginOrSignUpFormContainer'
 import { useLoginModalContext } from '../../provider/LoginModalProvider'
 
@@ -29,16 +29,55 @@ function Carousel(){
         'https://imgak.mmtcdn.com/pwa_v3/pwa_header_assets/loginPersuassionValley.webp'
     ]
     const carouselRef = useRef(null);
-    const [indexOfImage, setIndexOfImage] = useState(0);
+    const [index, setIndex] = useState(0)
+    const [isSmooth, setIsSmooth] = useState(true)
+    function scrollToIndexSmoothly(index, isSmooth) {
+        const divNode = carouselRef.current;
+        const imgNode = divNode.querySelectorAll('div > picture')[index];
+        imgNode.scrollIntoView({
+          behavior: isSmooth ? 'smooth' : 'instant',
+          block: 'nearest',
+          inline: 'center'
+        });
+      }
+      function handlePrev(){
+        // index == 0 ? setIndex(3) : setIndex(index-1);
+        if(index == 0){
+          setIndex(2);
+          setIsSmooth(false);
+        }
+        else{
+          setIndex(index-1)
+          setIsSmooth(true)
+        } 
+      }
+      function handleNext(){
+        // index == 3 ? setIndex(0) : setIndex(index+1); 
+        if(index == 2){
+          setIndex(0);
+          setIsSmooth(false)
+        }
+        else{
+          setIndex(index + 1)
+          setIsSmooth(true)
+        }
+      }
+    useEffect(()=>{
+        scrollToIndexSmoothly(index, isSmooth)
+    },[index])
     return(
         <>
             <div id="carousel" className='carousel-image-container' ref={carouselRef}>
                 {images.map((image,index)=>(
                     <picture key={index} className='carousel-picture'>
                         <img src={image} alt="" />
+                        <div  className='carousel-text'>Made by Alok</div>
+                        <div>
+                            <button onClick={handlePrev} class="carousel-scroll-left"></button>
+                            <button onClick={handleNext} class="carousel-scroll-right"></button>
+                        </div>
                     </picture>
                 ))}
-                <div className='carousel-text'>alok</div>
             </div>
         </>
     )
